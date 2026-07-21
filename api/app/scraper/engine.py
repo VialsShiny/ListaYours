2a973c2dae3b44cdce7a1b0453a4e921eef27cde
VialsShiny <150128459+VialsShiny@users.noreply.github.com>
2026-07-21 22:09:26 +0200

    feat(api): added screenshots debug

--- api/app/scraper/engine.py
+++ api/app/scraper/engine.py
@@ -2,6 +2,11 @@ import logging
 import httpx
 from playwright.async_api import async_playwright
 from app.scraper.parsers import extract_all_data
+from pathlib import Path
+from datetime import datetime
+
+FILES_DIR = Path("screenshots")
+FILES_DIR.mkdir(parents=True, exist_ok=True)
 
 logger = logging.getLogger("scraper_engine")
 
@@ -13,6 +18,8 @@ async def fetch_with_httpx(url: str) -> str:
         "Accept-Language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
     }
+    # filename = FILES_DIR / f"{datetime.now():%Y%m%d_%H%M%S}.png"
+    # await take_screenshot(url, filename)
     async with httpx.AsyncClient(timeout=15.0, follow_redirects=True) as client:
         response = await client.get(url, headers=headers)
         response.raise_for_status()
@@ -29,8 +36,20 @@ async def fetch_with_playwright(url: str) -> str:
         await page.goto(url, wait_until="domcontentloaded", timeout=30000)
         await page.wait_for_timeout(2000)
         content = await page.content()
+        # filename = FILES_DIR / f"{datetime.now():%Y%m%d_%H%M%S}.png"
+        # await take_screenshot(url, filename)
         await browser.close()
         return content
+    
+async def take_screenshot(url: str, path: Path):
+    path.parent.mkdir(parents=True, exist_ok=True)
+    logger.info(f"Saving screenshot: {path.resolve()}")
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=True)
+        page = await browser.new_page()
+        await page.goto(url, wait_until="networkidle", timeout=30000)
+        await page.screenshot(path=str(path), full_page=True)
+        await browser.close()
 
 async def scrape_product(url: str) -> tuple[dict, str]:
     strategy = "HTTPX"
